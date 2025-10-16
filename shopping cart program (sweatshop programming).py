fd9afa68d89d8e75f5ca41cfaf336846c51f65c4
ECTSLiamCool <batteryboythebest@gmail.com>
2025-10-16 08:53:47 -0400

    shopping cart
    i love capitalism

--- shopping cart program (sweatshop programming).py
+++ shopping cart program (sweatshop programming).py
@@ -1,8 +1,52 @@
-foods = ["pizza", "hamburger", "Hot Dog"]
-prices = ["$5.99", "$3.50", "$1.75"]
+import math
 
-"pizza" = "$5.99", "hamburger" = "$3.50", "Hot Dog" = "$1.75"
+def print_menu(foods:list[str], prices:list[int]):
+    """prints a table with 2 lists. Assumes list1 is string list2 is a list of ints"""
+    
 
-capitalism_brainwashing = True
-while capitalism_brainwashing == True:
-    food_buy = input("Enter a food to buy (Q to quit) > ")
+
+# def print_menu(foods, prices):
+#     """assumes 2 parallel arrays are passed in"""
+#     for i in range(len(foods)):
+#         print(f"{foods[i]}: {prices[i]}")
+def main():
+    foods = []
+    prices = []
+
+    total = 0.0
+
+    print_menu(foods, prices)
+    while True:
+        userinput = input("What would you like to buy? Or press Q to quit -> ").strip()
+        if userinput.lower() == 'q':
+            break
+        
+        while True:
+            priceinput = input(f"Enter the price for '{userinput}': ").strip()
+            
+            try:
+                priceinput = float(priceinput)
+                break
+            except ValueError:
+                print("Invalid Input, try again.")
+                continue
+        foods.append(userinput)
+        prices.append(priceinput)
+        total += priceinput
+    
+    print (f"           YOUR CART")        
+    print("*" * 29)
+    print(f"*  Food:           | Price: *")
+    print("*" * 29)
+    for idx in range(len(foods)):
+        #note this assumes the parallel lists have the same number of elements. 
+        print (f"*  {str(foods[idx]).ljust(15)} | ${str(prices[idx]).ljust(7)}*")
+    print("*" * 29)
+
+
+    print(f"Total Cost:  ${total:.2f}\n")
+
+
+
+if __name__ == "__main__":
+    main()
